8dafe1159bda9021c7871caf201e5212df2f34e0
Yoel Augustan <145452333+Yoelaugustan@users.noreply.github.com>
2024-12-07 23:31:16 +0700

    Delete static/script2.js

--- static/script2.js
+++
@@ -1,9 +0,0 @@
-let mediaRecorder;
-let audioChunks = [];
-const micButton = document.querySelector(".mic-button");
-const popup = document.getElementById("micPopup");
-const closePopupButton = document.getElementById("closePopup");
-const recordingStatus = document.getElementById("recordingStatus");
-const messageInput = document.getElementById("messageInput");
-const sendMessageBtn = document.getElementById("sendMessageBtn");
-const messagesContainer = document.querySelector(".chat-container");
